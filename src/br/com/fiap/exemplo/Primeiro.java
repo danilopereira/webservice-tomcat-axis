fc19011ec3f0c94a445b68459f954f2cf762be55
danilopereira <dansilva13@gmail.com>
2017-04-06 22:00:09 -0300

    Project strutcture

---
+++ src/br/com/fiap/exemplo/Primeiro.java
@@ -0,0 +1,12 @@
+package br.com.fiap.exemplo;
+
+public class Primeiro {
+	public String ola(String nome){
+		return "Olá "+nome;
+	}
+	
+	public String boanoite(String nome){
+		return "Boa noite "+nome;
+	}
+
+}
